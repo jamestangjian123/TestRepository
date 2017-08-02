e9b9e32f8bcbd91442dd463056fed4a8bf782c76
tangjian <tangjian@Ctrip.com>
2017-08-02 11:08:53 +0800

    222

--- WindowsFormsApplication4/WindowsFormsApplication4/Form1.Designer.cs
+++ WindowsFormsApplication4/WindowsFormsApplication4/Form1.Designer.cs
@@ -172,7 +172,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(593, 392);
+            this.ClientSize = new System.Drawing.Size(712, 421);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
